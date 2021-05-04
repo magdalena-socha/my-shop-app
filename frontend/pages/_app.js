a060778c22bf64ab1ca8dc688dd70e39590d65ef
Magdalena <sochamagdalena8@gmail.com>
2021-05-04 21:48:18 +0200

    fixing route changes

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,13 @@
   
 import Page from '../components/Page';
+import Nprogress from 'nprogress';
+import Router from 'next/router';
+import '../components/styles/nprogress.css';
+
+Router.events.on('routeChangeStart', ()=> Nprogress.start());
+Router.events.on('routeChangeComplete', ()=> Nprogress.done());
+Router.events.on('routeChangeError', ()=> Nprogress.done());
+
 
 export default function MyApp({ Component, pageProps }) {
   return (

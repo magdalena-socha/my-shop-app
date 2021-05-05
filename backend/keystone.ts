ed4a5d18c782772a0cb2541459366708e3521a2f
Magdalena <sochamagdalena8@gmail.com>
2021-05-05 23:55:53 +0200

    graphql setup

---
+++ backend/keystone.ts
@@ -0,0 +1,60 @@
+import {createAuth} from '@keystone-next/auth';
+import {withItemData, statelessSessions} from '@keystone-next/keystone/session';
+// import { config } from 'dotenv/types';
+import {config, createSchema} from '@keystone-next/keystone/schema';
+import {User} from './schemas/User';
+import {Product} from './schemas/Product';
+import {ProductImage} from './schemas/ProductImage';
+import 'dotenv/config';
+import { insertSeedData } from './seed-data';
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+    maxAge: 60*60*24,
+    secret: process.env.COOKIE_SECRET,
+};
+
+const {withAuth} = createAuth({
+    listKey: 'User',
+    identityField: 'email',
+    secretField: 'password',
+    initFirstItem: {
+        fields: ['name', 'email', 'password'],
+        //add in initial roles later
+    },
+});
+ 
+export default withAuth(config({
+    server: {
+        cors: {
+            origin: [process.env.FRONTEND_URL],
+            credentials: true,
+        },
+    },
+    db: {
+        adapter: 'mongoose',
+        url: databaseURL,
+        async onConnect(keystone) {
+            if (process.argv.includes('--seed-data')) {
+                await insertSeedData(keystone);
+            }
+            
+        },
+
+    },
+    lists: createSchema({
+        //schemaitems
+        User,
+        Product,
+        ProductImage,
+    }),
+    ui: {
+        //change this for roles
+        isAccessAllowed: ( {session }) => {
+            return !!session?.data;
+        },
+    },
+    session: withItemData(statelessSessions(sessionConfig),{
+        User: 'id',
+    })
+}))

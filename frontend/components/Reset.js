f29cbd01e738130e2b2b2d8feeadb650802934b3
Magdalena Socha <sochamagdalena8@gmail.com>
2021-05-24 20:21:28 +0200

    token request

---
+++ frontend/components/Reset.js
@@ -0,0 +1,70 @@
+import Form from "./styles/Form";
+import useForm from "../lib/useForm";
+import gql from 'graphql-tag';
+import { useMutation } from "@apollo/client";
+import DisplayError from './ErrorMessage';
+
+
+const RESET_MUTATION = gql`
+mutation RESET_MUTATION ($token: String!, $email: String!, $password: String!) {
+    redeemUserPasswordResetToken(email: $email, token: $token, password: $password) {
+        code
+        message
+    }
+}
+`;
+
+function Reset({token}) {
+    const {inputs, handleChange, resetForm} = useForm({
+        email: '',
+        password: '',
+        token,
+    });
+
+    const [reset, { data, loading, error}] = useMutation(RESET_MUTATION, {
+        variables: inputs,
+    });
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const res = await reset();
+        console.log(res);
+        resetForm();
+    }
+    
+    const anotherError = data?.redeemUserPasswordResetToken?.code ? data?.redeemUserPasswordResetToken : undefined;
+
+    return (
+        <Form method="post" onSubmit={handleSubmit}>
+            <h2>Reset your password</h2>
+            <DisplayError error={error || anotherError}/>
+            <fieldset>
+                <label htmlFor="email">
+                    Email
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Your email address"
+                        autoComplete="email"
+                        value={inputs.email}
+                        onChange={handleChange}
+                        />
+                </label>
+                <label htmlFor="password">
+                    Password
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="password"
+                        autoComplete="password"
+                        value={inputs.password}
+                        onChange={handleChange}
+                        />
+                </label>
+                <button type="submit">Request</button>
+            </fieldset>
+        </Form>
+    )
+}
+
+export default Reset
---
+++ frontend/pages/reset.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import RequestReset from '../components/RequestReset'
+import Reset from '../components/Reset'
+
+function ResetPage({query}) {
+    if(!query?.token) {
+        return (
+            <div>
+                <p>No token found</p>
+                <RequestReset/>
+            </div>
+        
+        )
+    }
+    return (
+        <div>
+            <p>reset</p>
+            <Reset token={query.token}/>
+        </div>
+    )
+}
+
+export default ResetPage
